a5b5683ed2cc3c027359d1800ac13588a8073f39
Catalin <bardascat@gmail.com>
2018-11-18 18:01:53 +0200

    fixed swagger

--- rest/engagement-bot.py
+++ rest/engagement-bot.py
@@ -1,38 +1,42 @@
-import subprocess
 import os
+import subprocess
+
 from rest_logger import getLogger
-import json
+
 python_path = "python"
 base_path = "/home/ubuntu/projects/InstaPy"
 DEVNULL = open(os.devnull, 'wb')
 
+
 def readAll():
-    #todo ->
+    # todo ->
     x = 1
 
 
-def start(body):
+def start(campaign):
     logger = getLogger()
-    bodyObject = json.loads(body)
-    logger.info(body)
-    id_campaign = bodyObject["id_campaign"]
-    logger.info("engagement-bot.start: Going to start bot for campaign id: %s", id_campaign)
+    logger.info("engagement-bot.start: Going to start bot for campaign id: %s", campaign['id_campaign'])
+    id_campaign = campaign["id_campaign"]
 
-    processName = 'angie_instapy_idc'+str(id_campaign)
-    subprocess.Popen("bash -c \"exec -a " + processName + " python "+base_path+"/start.py  -angie_campaign=" + str(id_campaign) + " \"", stdin=None, stdout=DEVNULL, stderr=DEVNULL, close_fds=True, shell=True)
+    processName = 'angie_instapy_idc' + str(id_campaign)
+    subprocess.Popen("bash -c \"exec -a " + processName + " python " + base_path + "/start.py  -angie_campaign=" + str(
+        id_campaign) + " \"", stdin=None, stdout=DEVNULL, stderr=DEVNULL, close_fds=True, shell=True)
+
 
 def getBot(id_campaign):
-    #todo ->
+    # todo ->
     x = 1
-
 
 
 def scheduler(id_campaigns):
     processName = 'angie_schedule_instapy_bot_to_start'
-    subprocess.Popen("bash -c \"exec -a " + processName + " python " + base_path + "/start.py  -angie_campaign=" + str(id_campaigns) + " \"", stdin=None, stdout=DEVNULL, stderr=DEVNULL, close_fds=True, shell=True)
+    subprocess.Popen("bash -c \"exec -a " + processName + " python " + base_path + "/start.py  -angie_campaign=" + str(
+        id_campaigns) + " \"", stdin=None, stdout=DEVNULL, stderr=DEVNULL, close_fds=True, shell=True)
+
 
 def stopAll():
-    x=1
+    x = 1
     DEVNULL = open(os.devnull, 'wb')
     processName = 'angie_stop_bots'
-    subprocess.Popen("bash -c \"exec -a " + processName + " python " + base_path + "/stop_bot.py " + " \"", stdin=None, stdout=DEVNULL, stderr=DEVNULL, close_fds=True, shell=True)
+    subprocess.Popen("bash -c \"exec -a " + processName + " python " + base_path + "/stop_bot.py " + " \"", stdin=None,
+                     stdout=DEVNULL, stderr=DEVNULL, close_fds=True, shell=True)
