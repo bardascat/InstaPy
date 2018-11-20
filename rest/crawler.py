e9490d1dff3f1e789879955d1980b11c15ac02e5
Catalin <bardascat@gmail.com>
2018-11-20 15:51:09 +0200

    enable firefox

--- rest/crawler.py
+++ rest/crawler.py
@@ -16,7 +16,20 @@ def scanFeed(campaign):
     logger.info("crawler.scanFeed: Going to start feed crawler, id_campaign: %s", id_campaign)
 
     processName = 'angie_scan_user_feed_' + str(id_campaign)
-    command = "bash -c \"exec -a " + processName + " python " + base_path + "/scan_user_feed.py  -angie_campaign=" + str(id_campaign) + " \""
+    command = "bash -c \"exec -a " + processName + " python " + base_path + "/scan_user_feed.py  -angie_campaign=" + str(
+        id_campaign) + " \""
+
+    logger.info("executing command: %s", command)
+    subprocess.Popen(command, close_fds=True, shell=True, stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+
+
+def scanUserProfile(campaign):
+    id_campaign = campaign['id_campaign']
+    logger = getLogger()
+    logger.info("crawler.scanUserProfile: Going to start profile crawler, id_campaign: %s", id_campaign)
+
+    processName = 'angie_scan_users_profile_' + str(id_campaign)
+    command = "bash -c \"exec -a " + processName + " python " + base_path + "/scan_user_profile.py  -angie_campaign=" + str(id_campaign) + " \""
 
     logger.info("executing command: %s", command)
     subprocess.Popen(command, close_fds=True, shell=True, stdin=None, stdout=DEVNULL, stderr=DEVNULL)
