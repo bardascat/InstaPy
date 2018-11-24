eaf8e82d2d28b1fcff6497118e957b83380a4c21
bardascat@gmail.com <bardascat@gmail.com>
2018-11-24 16:08:25 +0200

    added feed crawler

--- verify_account.py
+++ verify_account.py
@@ -1,24 +1,3 @@
-import argparse
-import codecs
-import json
-import os
-import sys
-import traceback
-import  time
-result = {}
-try:
-    from instapy import InstaPy
-    from instapy import InstaPy
-    from instapy.bot_action_handler import getAmountDistribution, getActionAmountForEachLoop
-    from instapy.bot_util import *
-    from instapy.account_privacy_service import AccountPrivacyService
+from pymongo import MongoClient
 
-    result['status'] = False
-except:
-    exceptionDetail = traceback.format_exc()
-    # print(exceptionDetail)
-    #session.logger.critical("start: FATAL ERROR: %s", exceptionDetail)
-    result['exception'] = exceptionDetail
-finally:
-    print(json.dumps(result))
-    # session.logger.info("start:finally: Closing process...")
+print("works fine")
