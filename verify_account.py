abf624f407374277fcd32533d16c8894171212e6
bardascat@gmail.com <bardascat@gmail.com>
2018-11-24 15:45:24 +0200

    added feed crawler

--- verify_account.py
+++ verify_account.py
@@ -5,25 +5,26 @@ import os
 import sys
 import traceback
 
-from instapy import InstaPy
-from instapy.bot_util import *
-from instapy.account_privacy_service import AccountPrivacyService
-
-stdout = sys.stdout
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-sys.path.append(os.path.join(sys.path[0], '../'))
-
-parser = argparse.ArgumentParser(add_help=True)
-parser.add_argument('-settings', type=str, help="settings")
-args = parser.parse_args()
-
-if args.settings is None:
-    exit("verify_account: settings are not specified !")
-
-result = {}
-result['status'] = False
-
 try:
+    from instapy import InstaPy
+    from instapy.bot_util import *
+    from instapy.account_privacy_service import AccountPrivacyService
+
+    stdout = sys.stdout
+    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+    sys.path.append(os.path.join(sys.path[0], '../'))
+
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('-settings', type=str, help="settings")
+    args = parser.parse_args()
+
+    if args.settings is None:
+        exit("verify_account: settings are not specified !")
+
+    result = {}
+    result['status'] = False
+
+
     settings = json.loads(args.settings)
     campaign = fetchOne(
         "select ip,username,password,campaign.timestamp,id_campaign,id_user  from campaign left join ip_bot using (id_ip_bot) where id_campaign=%s",
