79f5212bea5ce23788a2f211b96bffa5bba7180d
bardascat@gmail.com <bardascat@gmail.com>
2018-11-24 15:48:45 +0200

    added feed crawler

--- verify_account.py
+++ verify_account.py
@@ -6,6 +6,11 @@ import sys
 import traceback
 
 try:
+    from instapy import InstaPy
+    from instapy import InstaPy
+    from instapy.bot_action_handler import getAmountDistribution, getActionAmountForEachLoop
+    from instapy.bot_util import *
+    from instapy.account_privacy_service import AccountPrivacyService
     result = {}
     result['status'] = False
 except:
