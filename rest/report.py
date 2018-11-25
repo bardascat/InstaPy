5898715a88af81bfae6adfea581b0b17dd42d1c3
bardascat@gmail.com <bardascat@gmail.com>
2018-11-25 18:37:22 +0200

    added feed crawler

--- rest/report.py
+++ rest/report.py
@@ -18,22 +18,21 @@ def getActionsGroupByOperation(id_campaign, start, end):
     gte = gte.replace(minute=0, hour=0, second=0, microsecond=0)
     lte = lte.replace(minute=59, hour=23, second=59, microsecond=999)
 
-    logger.info("report.search: Bot Action report by operation: campaign: %s, start: %s, end: %s" % (id_campaign, gte, lte))
+    logger.info(
+        "report.search: Bot Action report by operation: campaign: %s, start: %s, end: %s" % (id_campaign, gte, lte))
 
     client = getMongoConnection()
     db = client.angie_app
     pipeline = [
-        {"$match": {"id_campaign": int(id_campaign)}},
+        {"$match": {"id_campaign": int(id_campaign), "timestamp": {"$gte": gte, "$lte": lte}}},
         {"$group": {"_id": "$bot_operation", "total_action": {"$sum": 1}}},
         {"$project": {"_id": 0, "bot_operation": "$_id", "total_action": 1}}
     ]
-
 
     result = db.bot_action.aggregate(pipeline=pipeline)
     result = list(result)
 
     logger.info("report: Retrieved %s rows", len(result))
-    logger.info("report: result: %s", result)
     client.close()
 
     return result
