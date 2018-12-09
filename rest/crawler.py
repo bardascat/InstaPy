87354082f5199101f2e43d8dc227017bcfeb7eeb
Catalin <bardascat@gmail.com>
2018-12-09 23:00:53 +0200

    new api

--- rest/crawler.py
+++ rest/crawler.py
@@ -37,23 +37,33 @@ def scanUserProfile(campaign):
 def getPostsByHashtag(hashtag, amount, id_campaign, removeLikedPosts, removeFollowedUsers):
     id_campaign_bot = 401 #florin_geambasu
 
-    #logger = getLogger()
-    print("crawler.getPostsByHashtag: Going to search posts by hashtag with options: hashtag: %s, amount: %s, id_campaign: %s" % (hashtag, amount, id_campaign))
+    logger = getLogger()
+    logger.info("crawler.getPostsByHashtag: Going to search posts by hashtag with options: hashtag: %s, amount: %s, id_campaign: %s" % (hashtag, amount, id_campaign))
 
     processName = 'angie_scan_posts_by_hashtag_' + str(id_campaign)
     command = "bash -c \"exec -a " + processName + " sudo /usr/bin/python2.7 " + base_path + "/get_posts_by_hashtag.py  -id_campaign=" + str(id_campaign) + "  -id_bot=" + str(id_campaign_bot) + " -amount=" + str(amount) + " -hashtag=" + hashtag+ " -removeLikedPosts=" + removeLikedPosts + " -removeFollowedUsers=" + removeFollowedUsers + "\""
 
-    print("executing command: %s", command)
-    subprocess.Popen(command, close_fds=True, shell=True, stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+    logger.info("executing command: %s", command)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    result = process.communicate()[0]
+    process.wait()
+
+    logger.info("crawler.getPostsByHashtag: Result is: %s", result)
+    return result
 
 def getPostsByLocation(location, amount, id_campaign, removeLikedPosts, removeFollowedUsers):
     id_campaign_bot = 401 #florin_geambasu
 
-    #logger = getLogger()
-    print("crawler.getPostsByHashtag: Going to search posts by location with options: location: %s, amount: %s, id_campaign: %s" % (location, amount, id_campaign))
+    logger = getLogger()
+    logger.info("crawler.getPostsByHashtag: Going to search posts by location with options: location: %s, amount: %s, id_campaign: %s" % (location, amount, id_campaign))
 
-    processName = 'angie_scan_posts_by_hashtag_' + str(id_campaign)
+    processName = 'angie_scan_posts_by_location_' + str(id_campaign)
     command = "bash -c \"exec -a " + processName + " sudo /usr/bin/python2.7 " + base_path + "/get_posts_by_location.py  -id_campaign=" + str(id_campaign) + "  -id_bot=" + str(id_campaign_bot) + " -amount=" + str(amount) + " -location=" + location+ " -removeLikedPosts=" + removeLikedPosts + " -removeFollowedUsers=" + removeFollowedUsers + "\""
 
-    print("executing command: %s", command)
-    subprocess.Popen(command, close_fds=True, shell=True, stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+    logger.info("executing command: %s", command)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    result = process.communicate()[0]
+    process.wait()
+
+    logger.info("crawler.getPostsByLocation: Result is: %s", result)
+    return result
