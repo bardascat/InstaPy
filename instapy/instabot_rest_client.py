d1aae143fa1a216bf63e80bb16054208fad666b5
Catalin <bardascat@gmail.com>
2018-12-10 12:37:12 +0200

    enable firefox

--- instapy/instabot_rest_client.py
+++ instapy/instabot_rest_client.py
@@ -12,8 +12,10 @@ import time
 import bot_action_handler
 from datetime import datetime
 import urllib2
+import urllib
 import requests
 import json
+
 
 class InstabotRestClient:
     def __init__(self,
@@ -27,12 +29,21 @@ class InstabotRestClient:
 
     def getPostsByHashtag(self, hashtag, amount):
 
-        #todo: if not enough posts are retrieve from instabot try to move the difference to other tags
+        # todo: if not enough posts are retrieve from instabot try to move the difference to other tags
 
-        removeFollowedUsers= True #todo set the right value for this
-        removeLikedPosts = True  #todo set the right value for this
+        removeFollowedUsers = True  # todo set the right value for this
+        removeLikedPosts = True  # todo set the right value for this
 
-        url="http://35.166.100.155:5000/api/crawler/post/hashtag?id_campaign="+str(self.campaign['id_campaign'])+"&amount="+str(amount)+"&hashtag="+hashtag+"&removeLikedPosts="+str(removeLikedPosts).lower()+"&removeFollowedUsers="+str(removeFollowedUsers).lower()
+        url = "http://35.166.100.155:5000/api/crawler/post/hashtag"
+        parameters = {}
+        parameters['id_campaign'] = self.campaign['id_campaign']
+        parameters['amount'] = amount
+        parameters['hashtag'] = hashtag
+        parameters['removeLikedPosts'] = str(removeLikedPosts).lower()
+        parameters['removeFollowedUsers'] = str(removeFollowedUsers).lower()
+
+        urlValues = urllib.urlencode(parameters)
+        url = url + urlValues
 
         self.logger.info("getPostsByHashtag: API URL: %s:", url)
 
@@ -43,11 +54,14 @@ class InstabotRestClient:
         if "error" in result:
             # maybe send an email with the error
             # self.browser.get('https://rest.angie.one/email/sendError?id=' + str(campaign['id_user']))
-            self.instapy.logger.error("get_links: Error while getting links for tag: %s, error: %s" % (hashtag, result['error']))
+            self.instapy.logger.error(
+                "get_links: Error while getting links for tag: %s, error: %s" % (hashtag, result['error']))
             return []
 
         if len(result) < amount:
-            self.instapy.logger.error("instabot_rest_client.getPostsByHashtag: Not enough links received for tag: %s. Expected: %s, received: %s" % (hashtag, amount, len(result)))
+            self.instapy.logger.error(
+                "instabot_rest_client.getPostsByHashtag: Not enough links received for tag: %s. Expected: %s, received: %s" % (
+                hashtag, amount, len(result)))
 
         return result
 
@@ -55,7 +69,18 @@ class InstabotRestClient:
         removeFollowedUsers = True  # todo set the right value for this
         removeLikedPosts = True  # todo set the right value for this
 
-        url = "http://35.166.100.155:5000/api/crawler/post/hashtag?id_campaign=" + str(self.campaign['id_campaign']) + "&amount=" + str(amount) + "&location=" + location + "&removeLikedPosts=" + str(removeLikedPosts).lower() + "&removeFollowedUsers=" + str(removeFollowedUsers).lower()
+        url = "http://35.166.100.155:5000/api/crawler/post/hashtag"
+
+        parameters = {}
+        parameters['id_campaign'] = self.campaign['id_campaign']
+        parameters['amount'] = amount
+        parameters['location'] = location
+        parameters['removeLikedPosts'] = str(removeLikedPosts).lower()
+        parameters['removeFollowedUsers'] = str(removeFollowedUsers).lower()
+
+        urlValues = urllib.urlencode(parameters)
+        url = url + urlValues
+
 
         self.logger.info("getPostsByLocation: API URL: %s:", url)
         req = urllib2.Request(url)
@@ -67,11 +92,13 @@ class InstabotRestClient:
         if "error" in result:
             # maybe send an email with the error
             # self.browser.get('https://rest.angie.one/email/sendError?id=' + str(campaign['id_user']))
-            self.instapy.logger.error("get_links: Error while getting links for location: %s, error: %s" % (location, result['error']))
+            self.instapy.logger.error(
+                "get_links: Error while getting links for location: %s, error: %s" % (location, result['error']))
             return []
 
         if len(result) < amount:
-            self.instapy.logger.error("instabot_rest_client.getPostsByHashtag: Not enough links received for tag: %s. Expected: %s, received: %s" % (hashtag, amount, len(result)))
+            self.instapy.logger.error(
+                "instabot_rest_client.getPostsByHashtag: Not enough links received for tag: %s. Expected: %s, received: %s" % (
+                hashtag, amount, len(result)))
 
         return result
-
