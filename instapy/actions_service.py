5be27c817a00e7dccd0911de6721515b8d892d4a
Catalin <bardascat@gmail.com>
2019-02-06 23:46:59 +0200

    action service

---
+++ instapy/actions_service.py
@@ -0,0 +1,273 @@
+import time
+from random import randint
+
+from selenium.common.exceptions import NoSuchElementException
+
+from .api_db import *
+from .bot_util import getIfUserWantsToUnfollow, isFollowEnabled
+from .like_util import like_image
+from .unfollow_util import custom_unfollow, follow_user
+from random import randint
+
+
+class ActionsService:
+    def __init__(self,
+                 campaign,
+                 instapy
+                 ):
+        self.campaign = campaign
+        self.instapy = instapy
+        self.browser = instapy.browser
+        self.logger = instapy.logger
+        self.isFollowEnabled = isFollowEnabled(campaign['id_campaign'], self.logger)
+        self.isUnfollowEnabled = getIfUserWantsToUnfollow(campaign['id_campaign'])
+
+    def perform_engagement(self, likeAmount, followAmount, unfollowAmount):
+
+        result = {"like": 0, "follow": 0, "unfollow": 0}
+
+        self.logger.info("perform_engagement: Going to perform %s likes, %s follow, %s Unfollow." % (
+            likeAmount, followAmount, unfollowAmount))
+
+        numberOfPostsToInteractWith = max(likeAmount, followAmount, unfollowAmount)
+        self.followProbabilityPercentage = followAmount * 100 // numberOfPostsToInteractWith
+        self.likeProbabilityPercentage = likeAmount * 100 // numberOfPostsToInteractWith
+        self.unfollowProbabilityPercentage = unfollowAmount * 100 // numberOfPostsToInteractWith
+
+        posts = self.getPosts(numberOfPostsToInteractWith)
+        it = 0
+        for post in posts:
+
+            if 'link' not in post:
+                continue
+
+            self.logger.info("perform_engagement: [%s][%s] link: %s" % (it, len(posts), post['link']))
+            try:
+                actionStatus = self.engage(post)
+
+                if actionStatus['like']:
+                    result['like'] += 1
+                if actionStatus['follow']:
+                    result['follow'] += 1
+                if actionStatus['unfollow']:
+                    result['unfollow'] += 1
+                self.disablePost(post)
+
+            except NoSuchElementException as err:
+                self.logger.error('engage: Invalid Page: {}'.format(err))
+                self.disablePost(post)
+                continue
+            it += 1
+
+            # pause once at 100 posts
+            if it % 100 == 0:
+                self.logger.info("perform_engagement: Iteration: %s Going to wait for %s minutes" % (it, wait))
+                wait = randint(7, 15)
+                time.sleep(wait * 60)
+
+            self.logger.info("perform_engagement: Done waiting...")
+
+        return result
+
+    def disablePost(self, post):
+        client = getMongoConnection()
+        db = client.angie_app
+        db.user_actions_queue.update({"_id": post["_id"]}, {"$set": {"processed": 1}})
+        client.close()
+
+    def getPosts(self, noPosts):
+
+        client = getMongoConnection()
+        db = client.angie_app
+        # sort them asc
+        result = db.user_actions_queue.find({"id_campaign": self.campaign['id_campaign'], "processed": 0},
+                                            sort=[("timestamp", 1)]).limit(noPosts)
+        client.close()
+
+        if result is None:
+            self.logger.error("getPosts: Could not find any posts to engage with, going to return")
+            return False
+
+        result = list(result)
+        self.logger.info("getPosts: Found %s posts for this user, expected: %s posts." % (len(result), noPosts))
+
+        return result
+
+    def engage(self, post):
+
+        self.browser.get(post['link'])
+
+        operation = self.getOperationName(post)
+
+        self.logger.info("engage: Trying to like post %s", post['link'])
+        likeStatus = self.performLike(user_name=post['instagram_username'],
+                                      operation=operation,
+                                      link=post['link'],
+                                      engagementValue=post['tag'])
+
+        self.logger.info("engage: Trying to follow user: %s", post['instagram_username'])
+
+        followStatus = self.performFollow(followAmountProbabilityPercentage=self.followProbabilityPercentage,
+                                          link=post['link'],
+                                          operation=operation,
+                                          user_name=post['instagram_username'],
+                                          tag=post['tag'])
+
+        self.logger.info("engage: Trying to unfollow an user from database...")
+
+        unfollowStatus = self.performUnfollow(unFollowAmountProbabilityPercentage=self.unfollowProbabilityPercentage, operation=operation)
+
+        return {"like": likeStatus, "follow": followStatus, "unfollow": unfollowStatus}
+
+    def getOperationName(self, post):
+        if post['targetType'] == "hashtag":
+            operation = "engagement_by_hashtag"
+        elif post['targetType'] == "location":
+            operation = "engagement_by_location"
+        return operation
+
+    def performLike(self, user_name, link, operation, engagementValue):
+        # if self.totalLikePerformed >= self.totalLikeExpected:
+        #     self.logger.error("performLike: ERROR - The like amount is reached. Expected %s, performed %s " % (
+        #         self.totalLikeExpected, self.totalLikePerformed))
+        #     return False
+        liked, msg = like_image(self.browser,
+                                user_name,
+                                self.instapy.blacklist,
+                                self.logger,
+                                self.instapy.logfolder,
+                                self.instapy)
+        if liked:
+            self.logger.info("performLike: Link %s was liked. User %s" % (link, user_name))
+
+        if msg == "already_liked":
+            self.logger.info("performLike: Link %s was already liked, going to store it in db.", link)
+
+        if liked is True or msg == "already_liked":
+            insertBotAction(self.campaign['id_campaign'], self.campaign['id_user'],
+                            None, None, user_name,
+                            None, None, None,
+                            link, 'like_' + operation, engagementValue, self.instapy.id_log)
+            self.logger.info("performLike: Going to sleep 3 seconds after jumping to other page...")
+            time.sleep(3)
+            return True
+
+        return False
+
+    def performFollow(self, followAmountProbabilityPercentage, link, user_name, tag, operation):
+
+        if user_name is None:
+            self.logger.error("performFollow: Cannot follow user: %s ", user_name)
+            return False
+
+        # if self.totalFollowPerformed >= self.totalFollowExpected:
+        #     self.logger.error("performFollow: ERROR - The follow amount is reached. Expected %s, performed %s " % (
+        #         self.totalFollowExpected, self.totalFollowPerformed))
+        #     return False
+
+        if self.isFollowEnabled:
+            randomProbability = randint(0, 100)
+
+            self.logger.info("performFollow: Probability to follow: %s. Random Probability: %s" % (
+                followAmountProbabilityPercentage, randomProbability))
+            if randomProbability <= followAmountProbabilityPercentage:
+                # try to folllow
+                self.logger.info("performFollow: Trying to follow user %s", user_name)
+
+                # this is not necessary since the posts are prefiltered
+                # # check if user was followed in the past
+                # if userWasFollowedInThePast(user_name, self.campaign['id_user']):
+                #     self.logger.error("performFollow: Warning: User %s was followed in the past, going to skip it !",
+                #                       user_name)
+                #     return False
+
+                # todo: follow_user method is overengineered , try to simplify it
+
+                followed, msg = follow_user(self.browser,
+                                            "post",
+                                            self.campaign['username'],
+                                            user_name,
+                                            None,
+                                            self.instapy.blacklist,
+                                            self.logger,
+                                            self.instapy.logfolder,
+                                            self.instapy)
+
+                if msg == "already_followed":
+                    self.logger.info("performFollow: User %s was already followed, going to store it in DB", user_name)
+
+                if followed is True or msg == "already_followed":
+                    insertBotAction(self.campaign['id_campaign'], self.campaign['id_user'],
+                                    None, None, user_name,
+                                    None, None, None,
+                                    link, 'follow_' + operation, tag, self.instapy.id_log)
+
+                    self.logger.info("performFollow: Going to sleep 3 seconds after jumping to other page...")
+                    time.sleep(3)
+
+                    return True
+                else:
+                    self.logger.error(
+                        "peformFollow: Error could not perform follow for user %s, reason: %s" % (user_name, msg))
+                    return False
+            else:
+                self.logger.info(
+                    "performFollow: Going to skip follow. Actual Probability: %s, random probability: %s" % (
+                        followAmountProbabilityPercentage, randomProbability))
+
+            return False
+
+    def performUnfollow(self, unFollowAmountProbabilityPercentage, operation):
+        # todo check if there is user to unfollow
+        # if self.totalUnfollowPerformed >= self.totalUnfollowExpected:
+        #     self.logger.error("performLike: ERROR - The unfollow amount is reached. Expected %s, performed %s " % (
+        #         self.totalUnfollowExpected, self.totalUnfollowPerformed))
+        #     return False
+
+        # check if user wants to unfollow
+        userWantsToUnfollow = getIfUserWantsToUnfollow(self.campaign['id_campaign'])
+        if userWantsToUnfollow == False:
+            self.logger.info("performUnfollow: User does not want to unfollow, going to continue !")
+            return False
+
+        randomProbability = randint(0, 100)
+
+        self.logger.info("performUnfollow: Probability to unfollow: %s. Random Probability: %s" % (
+            unFollowAmountProbabilityPercentage, randomProbability))
+
+        if randomProbability <= unFollowAmountProbabilityPercentage:
+            self.logger.info("performUnfollow: User wants to unfollow after %s hours" % userWantsToUnfollow['value'])
+
+            # get users to unfollow older than x days. People who did not follow back are the first to be unfollowed.
+            recordToUnfollow = getUserToUnfollow(self.campaign['id_user'], userWantsToUnfollow['value'])
+
+            if recordToUnfollow:
+                status = custom_unfollow(self.browser, recordToUnfollow['username'], self.logger, self.instapy)
+                if status is True:
+                    lastBotAction = insertBotAction(self.campaign['id_campaign'], self.campaign['id_user'],
+                                                    None, None, recordToUnfollow['username'],
+                                                    None, None, None, None, 'unfollow_' + operation,
+                                                    None,
+                                                    self.instapy.id_log)
+
+                    self.logger.info("performUnfollow: Succesfully unfollowed user: %s", recordToUnfollow['username'])
+
+                    revertBotFollow(recordToUnfollow['_id'], lastBotAction)
+                    self.logger.info("peformUnfolow: Update bot_operation_reverted with value %s for id: %s" % (
+                        lastBotAction, recordToUnfollow['_id']))
+                    self.logger.info("performFollow: Going to sleep 3 seconds after jumping to other page...")
+                    time.sleep(3)
+                    return True
+                else:
+                    self.logger.info("performFollow: Could not unfollow user...")
+                    return False
+
+            else:
+                self.logger.info("performUnfollow: No user found in database to unfollow...")
+
+        else:
+            self.logger.info(
+                "performUnfollow: Going go skip UNFOLLOW. Actual Probability: %s, random probability: %s" % (
+                    unFollowAmountProbabilityPercentage, randomProbability))
+
+        return False
---
+++ start_v2.py
@@ -0,0 +1,113 @@
+import argparse
+import codecs
+import os
+import sys
+from time import sleep
+from instapy import InstaPy
+from instapy.bot_action_handler import getAmountDistribution, getActionAmountForEachLoop
+from instapy.bot_util import *
+from instapy.account_privacy_service import AccountPrivacyService
+from instapy.exception_handler import ExceptionHandler
+import traceback
+
+stdout = sys.stdout
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.path.append(os.path.join(sys.path[0], '../'))
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-angie_campaign', type=str, help="angie_campaign")
+args = parser.parse_args()
+
+args.angie_campaign='1'
+
+if args.angie_campaign is None:
+    exit("dispatcher: Error: Campaign id it is not specified !")
+
+
+def start(session):
+    session.logger.info("start: ENGAGEMENT BOT STARTED for campaign: %s, with ip: %s. Going to try login..." % (
+        campaign['id_campaign'], campaign['ip']))
+    status = session.login()
+    if status == False:
+        exit("Could not  login")
+
+    # if no actions are set by the user
+    calculatedAmount = getAmountDistribution(session, args.angie_campaign)
+    if calculatedAmount['like_amount'] == 0 and calculatedAmount['follow_amount'] == 0:
+        session.logger.info("start: No actions set, going to exit. like_amount:%s, follow_amount:%s" % (
+        calculatedAmount['like_amount'], calculatedAmount['follow_amount']))
+        return False
+
+    totalExpectedLikeAmount = int(calculatedAmount["like_amount"])
+    totalExpectedFollowAmount = int(calculatedAmount["follow_amount"])
+    totalExpectedUnfollowAmount = int(calculatedAmount["unfollow_amount"])
+
+    # if amount of action was reached earlier this day
+    if session.engagementService.totalLikePerformed >= totalExpectedLikeAmount and session.engagementService.totalFollowPerformed >= totalExpectedFollowAmount and session.engagementService.totalUnfollowPerformed >= totalExpectedUnfollowAmount:
+        session.logger.info("start.py: going to break the MAIN loop. Number of actions reached for all ops !")
+        return False
+
+    operations = getBotOperations(campaign['id_campaign'], session.logger)
+    if getOperationsNumber(operations)==0:
+        session.logger.info("No operations of type engagement by hashtag/location enabled for this user.")
+        return False
+
+
+    session.logger.info("start: PID: %s, Instapy Started for account %s using proxy: %s" % (
+    os.getpid(), campaign['username'], campaign['ip']))
+
+    noOfLoops = randint(6, 8)
+
+    session.logger.info("start.py: Bot started performing actions: %s likes, %s follow, %s unfollow during %s loops" % (
+    totalExpectedLikeAmount, totalExpectedFollowAmount, totalExpectedUnfollowAmount, noOfLoops))
+    insert("INSERT INTO campaign_log (`id_campaign`, event, `details`, `timestamp`) VALUES (%s, %s, %s, now())",
+           campaign['id_campaign'], "ENGAGEMENT_BOT_STARTED_PERFORMING_ACTIONS", None)
+
+
+    iterationResults = session.actionService.perform_engagement(likeAmount=totalExpectedLikeAmount,
+                                                                followAmount=totalExpectedFollowAmount,
+                                                                unfollowAmount=totalExpectedUnfollowAmount)
+
+    session.logger.info("start: Done processing %s posts: result: %s" % (max(totalExpectedLikeAmount, totalExpectedFollowAmount, totalExpectedUnfollowAmount), iterationResults))
+    session.logger.info("start: Setting privacy to public for this account...")
+    accountPrivacyService = AccountPrivacyService(session)
+    accountPrivacyService.switchToPublic()
+
+    session.logger.info("start: ALL DONE, CLOSING ENGAGEMENT BOT")
+
+
+try:
+    campaign = fetchOne(
+        "select ip,username,password,campaign.timestamp,id_campaign,id_user  from campaign left join ip_bot using (id_ip_bot) where id_campaign=%s",
+        args.angie_campaign)
+    insert("INSERT INTO campaign_log (`id_campaign`, event, `details`, `timestamp`) VALUES (%s, %s, %s, now())",
+           campaign['id_campaign'], "ENGAGEMENT_BOT_STARTED", None)
+
+    if campaign['ip'] is None:
+        exit("Invalid proxy")
+
+    session = InstaPy(username=campaign['username'],
+                      password=campaign['password'],
+                      headless_browser=False,
+                      bypass_suspicious_attempt=False,
+                      proxy_address=campaign['ip'].replace("http://cata:lin@", ""),
+                      disable_image_load=False,
+                      campaign=campaign,
+                      proxy_port=80,
+                      multi_logs=True,
+                      bot_type="engagement_bot",
+                      force_login=False)
+
+    session.canBotStart(args.angie_campaign, "angie_instapy_idc")
+
+    session.set_quota_supervisor(enabled=True)
+    session.set_relationship_bounds(enabled=True, potency_ratio=0.01, max_followers=999999, max_following=99999,min_followers=100, min_following=50)
+
+    start(session)
+
+
+except Exception as exc:
+    exceptionDetail = traceback.format_exc()
+    print(exceptionDetail)
+    exceptionHandler = ExceptionHandler(session, 'engagement_bot')
+    exceptionHandler.handle(exc)
