7e5da422136ac126ff78fe6723aa8dd4bb8bb624
bardascat@gmail.com <bardascat@gmail.com>
2019-02-10 21:53:28 +0200

    stop bot

--- stop_bot.py
+++ stop_bot.py
@@ -28,6 +28,7 @@ def stopProcesses():
     os.system(" ps aux  |  grep -i angie_idc  |  awk '{print $2}'  |  xargs  kill -9")
     os.system("ps aux  |  grep -i angie_instapy |  awk '{print $2}'  |  xargs  kill -9")
     os.system("ps aux  |  grep -i angie_scan_user |  awk '{print $2}'  |  xargs  kill -9")
+    os.system("ps aux  |  grep -i angie_ |  awk '{print $2}'  |  xargs  kill -9")
     os.system("ps aux  |  grep -i chrome |  awk '{print $2}'  |  xargs  kill -9")
     print("final cleanup done...")
 
