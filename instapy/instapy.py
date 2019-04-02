9bcf1cd78148472c241048961b44d84706010a0b
Catalin <bardascat@gmail.com>
2019-04-02 15:19:33 +0300

    increased action delay

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -113,9 +113,9 @@ class InstaPy:
                  bypass_suspicious_attempt=False,
                  multi_logs=True,
                  force_login=False,
-                 like_delay=20,
-                 follow_delay=30,
-                 unfollow_delay=30,
+                 like_delay=30,
+                 follow_delay=35,
+                 unfollow_delay=35,
                  bot_type="engagement_bot"):
 
         if nogui:
