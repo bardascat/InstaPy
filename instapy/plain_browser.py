6aebfc0b6c891134bcaf03bc871f6e93c2ef2523
Ubuntu <ubuntu@ip-172-31-42-56.us-west-2.compute.internal>
2019-04-12 18:15:32 +0300

    fix

--- instapy/plain_browser.py
+++ instapy/plain_browser.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.proxy import Proxy, ProxyType
-
+import time
 chrome_options = Options()
 # chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("--mute-audio")
@@ -14,16 +14,20 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--enable-logging --v=1')
 chrome_options.add_argument('--log-path=/home/ubuntut/chromedriver.log')
 
-#chrome_options.add_argument('--headless')
-#chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
 
 chromedriver_location='/home/instapy-log/assets/chromedriver'
 
 browser = webdriver.Chrome(chromedriver_location,chrome_options=chrome_options)
 
 browser.get("http://google.ro")
+print("done google")
 browser.get("https://serverfault.com/questions/305524/memory-usage-by-bytes-top-10")
 browser.get("https://www.instagram.com/p/BvPK32FFxbz/")
 browser.get("https://www.instagram.com/p/BvPK32FFxbz2/")
 browser.get("https://www.instagram.com/p/BvPK32FFxb4/")
 browser.get("https://www.instagram.com/p/BvPK32FFxbz5/")
+
+print("sleeping 10 sec")
+time.sleep(10)
