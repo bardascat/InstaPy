5e390e68aab42e046f037aa96faa2f83cd075f49
Catalin <bardascat@gmail.com>
2019-05-13 19:00:55 +0300

    log message

--- instapy/bot_util.py
+++ instapy/bot_util.py
@@ -175,7 +175,7 @@ def getBotOperations(id_campaign, logger):
     # for op in operations:
     #     self.logger.info("Percentage: %s , Amount: %s" % (op['percentageAmount'], op['configName']))
 
-    logger.info("getBotOperations: Found %s operations: %s" % (len(operations), operations))
+    logger.info("getBotOperations: Found %s operations" % (len(operations)))
 
     for o in operations:
         logger.info("%s, enabled: %s" % (o['configName'], o['enabled']))
