3c35bcf0a202a35a2cf38946bc1b716dd06cb096
Catalin <bardascat@gmail.com>
2019-05-28 16:32:25 +0100

    switch_to_window fix

--- rest/l4l-dispatcher.py
+++ rest/l4l-dispatcher.py
@@ -7,12 +7,17 @@ python_path = "python"
 base_path = "/home/projects/InstaPy"
 DEVNULL = open(os.devnull, 'wb')
 
-logger = getLogger()
-logger.info("l4l-dispatcher.start: Starting like for like dispatcher")
+def start():
+    logger = getLogger()
+    logger.info("l4l-dispatcher.start: Starting like for like dispatcher")
 
-processName = 'angie_like_for_like_dispatcher'
+    processName = 'angie_like_for_like_dispatcher'
 
-command = "bash -c \"exec -a " + processName + " sudo /usr/bin/python2.7 " + base_path + "/l4l_dispatcher.py \""
+    command = "bash -c \"exec -a " + processName + " sudo /usr/bin/python2.7 " + base_path + "/l4l_dispatcher.py \""
 
-logger.info("executing command: %s", command)
-subprocess.Popen(command, close_fds=True, shell=True, stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+    logger.info("executing command: %s", command)
+    subprocess.Popen(command, close_fds=True, shell=True, stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+
+
+def getDispatcher():
+    x=1
