160f371669ad716b5d1d266d09741ad147ff90fd
Catalin <bardascat@gmail.com>
2019-05-31 13:47:31 +0100

    switch_to_window fix

--- instapy/api_db.py
+++ instapy/api_db.py
@@ -7,7 +7,7 @@ client = None
 
 
 def getMongoConnection():
-    client = MongoClient('mongodb://127.0.0.1/angie_app')
+    client = MongoClient('mongodb://104.248.128.65/angie_app')
     return client
 
 
